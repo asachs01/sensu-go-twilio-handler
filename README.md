13a655fc070b0aeea36319d857ccce049db085e1
Aaron Sachs <aaronm.sachs@gmail.com>
2019-07-17 09:04:26 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,35 +1,48 @@
-# Sensu Go CHANGEME Plugin
-[![Bonsai Asset Badge](https://img.shields.io/badge/CHANGEME-Download%20Me-brightgreen.svg?colorB=89C967&logo=sensu)](https://bonsai.sensu.io/assets/CHANGEME/CHANGEME) [![TravisCI Build Status](https://travis-ci.org/CHANGEME/sensu-CHANGEME.svg?branch=master)](https://travis-ci.org/CHANGEME/sensu-CHANGEME)
+# Sensu Go Twilio Handler Plugin
+[![Bonsai Asset Badge](https://img.shields.io/badge/Sensu%20Go%20Twilio%20Handler-Download%20Me-brightgreen.svg?colorB=89C967&logo=sensu)](https://bonsai.sensu.io/assets/asachs01/sensu-go-twilio-handler) [![TravisCI Build Status](https://travis-ci.org/asachs01/sensu-go-twilio-handler.svg?branch=master)](https://travis-ci.org/asachs01/sensu-go-twilio-handler)
 
-TODO: Description.
+This handler enables Sensu Go users to send SMS alerts using Twilio.
+
+TODO: SCREENSHOTS
 
 ## Installation
 
-Download the latest version of the sensu-CHANGEME from [releases][1],
+Download the latest version of the sensu-go-twilio-handler from [releases][1],
 or create an executable script from this source.
 
-From the local path of the sensu-CHANGEME repository:
+From the local path of the sensu-go-twilio-handler repository:
 
 ```
-go build -o /usr/local/bin/sensu-CHANGEME main.go
+go build -o /usr/local/bin/sensu-go-twilio-handler main.go
 ```
 
 ## Configuration
 
 Example Sensu Go definition:
-
+**JSON**
 ```json
 {
     "api_version": "core/v2",
-    "type": "CHANGEME",
+    "type": "Handler",
     "metadata": {
         "namespace": "default",
-        "name": "CHANGEME"
+        "name": "sensu-go-twilio-handler"
     },
     "spec": {
         "...": "..."
     }
 }
+```
+**YAML**
+```yaml
+---
+api_version: core/v2
+type: Handler
+metadata:
+  namespace: default
+  name: sensu-go-twilio-handler
+spec:
+  "...": "..."
 ```
 
 ## Usage Examples
@@ -37,18 +50,18 @@ Example Sensu Go definition:
 Help:
 
 ```
-The Sensu Go CHANGEME for x
+The Sensu Go Handler for Twilio
 
 Usage:
-  sensu-CHANGEME [flags]
+  sensu-go-twilio-handler [flags]
 
 Flags:
   -f, --foo string   example
-  -h, --help         help for sensu-CHANGEME
+  -h, --help         help for sensu-go-twilio-handler
 ```
 
 ## Contributing
 
 See https://github.com/sensu/sensu-go/blob/master/CONTRIBUTING.md
 
-[1]: https://github.com/CHANGEME/sensu-CHANGEME/releases
+[1]: https://github.com/asachs01/sensu-go-twilio-handler/releases
--- main.go
+++ main.go
@@ -4,17 +4,25 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/sensu/sensu-go/types"
 	"github.com/spf13/cobra"
 )
 
+//Declare our variable types here
 var (
-	foo   string
-	stdin *os.File
+	accountSid string
+	authToken  string
+	fromNumber string
+	recipient  string
+	stdin      *os.File
 )
 
+//Start main function
 func main() {
 	rootCmd := configureRootCommand()
 	if err := rootCmd.Execute(); err != nil {
@@ -23,20 +31,45 @@ func main() {
 	}
 }
 
+//Configure the root command and add our flags
 func configureRootCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "sensu-CHANGEME",
-		Short: "The Sensu Go CHANGEME for x",
+		Use:   "sensu-go-twilio-handler",
+		Short: "The Sensu Go Handler for Twilio",
 		RunE:  run,
 	}
 
-	cmd.Flags().StringVarP(&foo,
-		"foo",
+	cmd.Flags().StringVarP(&accountSid,
+		"accountSid",
+		"s",
+		os.Getenv("TWILIO_ACCOUNT_SID"),
+		"The account SID for your Twilio account. Uses the environment variable TWILIO_ACCOUNT_SID by default.")
+
+	_ = cmd.MarkFlagRequired("accountSid")
+
+	cmd.Flags().StringVarP(&authToken,
+		"authToken",
+		"t",
+		os.Getenv("TWILIO_AUTH_TOKEN"),
+		"The authorization token for your Twilio account. Uses the environment variable TWILIO_AUTH_TOKEN by default.")
+
+	_ = cmd.MarkFlagRequired("authToken")
+
+	cmd.Flags().StringVarP(&fromNumber,
+		"fromNumber",
 		"f",
 		"",
-		"example")
+		"Your Twilio phone number")
 
-	_ = cmd.MarkFlagRequired("foo")
+	_ = cmd.MarkFlagRequired("fromNumber")
+
+	cmd.Flags().StringVarP(&recipient,
+		"recipient",
+		"r",
+		"",
+		"The recipient's phone number")
+
+	_ = cmd.MarkFlagRequired("recipient")
 
 	return cmd
 }
@@ -70,13 +103,42 @@ func run(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("event does not contain check")
 	}
 
-	return exampleAction(event)
+	return nil
 }
 
-func exampleAction(event *types.Event) error {
-	fmt.Printf("hello world: %s\n", foo)
+func sendText(event *types.Event) error {
 
-	fmt.Printf("Event Key: %s-%s\n", event.Entity.Name, event.Check.Name)
+	//Set our API URL
+	urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
+
+	//Set up our message we want to send
+	msg := event.Check.Output
+
+	//Set up our message data
+	msgData := url.Values{}
+	msgData.Set("To", recipient)
+	msgData.Set("From", fromNumber)
+	msgData.Set("Body", msg)
+	msgDataReader := *strings.NewReader(msgData.Encode())
+
+	//Create the HTTP request client
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", urlStr, &msgDataReader)
+	req.SetBasicAuth(accountSid, authToken)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, _ := client.Do(req)
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		var data map[string]interface{}
+		decoder := json.NewDecoder(resp.Body)
+		err := decoder.Decode(&data)
+		if err == nil {
+			fmt.Println(data["sid"])
+		}
+	} else {
+		fmt.Println(resp.Status)
+	}
 
 	return nil
 }
